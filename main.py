8901ad0c124e995a1d0f1acef492caf976e9e793
DESKTOP-EC2N2LM\Tariq <t.riahi@student.utwente.nl>
2022-12-15 13:41:57 +0100

    Validation for consignments, goods and transportOrders.

--- main.py
+++ main.py
@@ -1,94 +1,32 @@
+import json
+
 import requests
 import streamlit as st
+import json
 
 
 # title
 st.write("OTM Validation Tool")
 
+# options
+option = st.selectbox(
+    'Which OTM message do you want to validate?',
+    ('consignments', 'transportOrders', 'goods'))
+
 # user input
 user_input = st.text_input("OTM message")
 
-st.write(user_input)
+# validate button
+validate = st.button('Validate', type='primary')
 
-# url = 'https://otm5-documentation-api.services.simacan.com/api/v5/validate/consignments'
-# myobj = {
-#     'goods': [
-#         {
-#             'entity' : {
-#                 'quantity': 1,
-#                 'type': 'items'
-#             },
-#             "associationType": "inline"
-#         },
-#     ],
-#     "actions": [
-#
-#         {
-#
-#             "entity":
-#
-#                 {
-#
-#                     "location":
-#
-#                         {
-#
-#                             "entity":
-#
-#                                 {
-#
-#                                     "geoReference":
-#
-#                                         {
-#                                             "street": "valutaboulevard",
-#                                             "houseNumber": "16",
-#                                             "postalCode": "1234AB",
-#                                             "type": "addressGeoReference"
-#                                         }
-#                                 },
-#                             "associationType": "inline"
-#                         },
-#                     "startTime": "2020-04-03T10:00:00Z",
-#                     "actionType": "load"
-#                 },
-#             "associationType": "inline"
-#
-#         },
-#         {
-#
-#             "entity":
-#
-#                 {
-#
-#                     "location":
-#
-#                         {
-#
-#                             "entity":
-#
-#                                 {
-#
-#                                     "geoReference":
-#
-#                                         {
-#                                             "street": "Boris Pasternaklaan",
-#                                             "houseNumber": "22",
-#                                             "postalCode": "9999ZZ",
-#                                             "type": "addressGeoReference"
-#                                         }
-#                                 },
-#                             "associationType": "inline"
-#                         },
-#                     "startTime": "2020-04-03T10:12:00Z",
-#                     "actionType": "unload"
-#                 },
-#             "associationType": "inline"
-#         }
-#
-#     ],
-# }
-#
-# x = requests.put(url, json = myobj)
-# print(x.ok)
-#
-# print("x:",x)
+
+if validate:
+    myobj = json.loads(user_input)
+
+    url = 'https://otm5-documentation-api.services.simacan.com/api/v5/validate/' + option
+    resp = requests.put(url, json = myobj)
+
+    if resp.ok:
+        st.write("Your OTM5 message is valid.")
+    else:
+        st.write(resp.text)
